d252c062d3a17d53c7eb68d2f1febfa7bad67cba
uaikhuel <89050866+uaikhuel@users.noreply.github.com>
2023-04-29 17:25:31 -0400

    Added Music, Video Demonstrations, New Functions
    I've added music to play during the stretches, a warm-up tutorial video as well as two more tutorial videos for the flexor and extensor stretches. I added VLC and OS imports to play the  music and videos repectively. This update also includes the functions needed for the videos and music to work.

--- HandTracker.py
+++ HandTracker.py
@@ -1,92 +1,111 @@
-
+import vlc
+import os
 import time
 from PIL import Image
+from os import *
 
+p = vlc.MediaPlayer("StretchMusic.mp3")
 countdownsec = 0
 
 print("Please schedule a good time for us to begin our stretch session:")
 time_hourmin = input("When would you like to schedule our next session?:")
 
-current_time = time.strftime("%H:%M:%S")
+current_time = time.strftime("%H:%M")
 print("The current time is", current_time)
 
-def countdown60(countdownsec):
+def startfile(fn):
+    os.system('open %s' % fn)
 
+def warmupvideotimer(countdownsec):
+    print("Please take some time to perform a few warm-ups before your stretch session:")
+    seconds = 120
+    for i in range(seconds):
+        print(str(seconds-i) + " seconds remaining \n")
+        time.sleep(1)
+    print("Time is up")
+
+def countdown60(countdownsec):
 
     seconds = 60
 
     for i in range(seconds):
         print(str(seconds-i) + " seconds remaining \n")
-
-    time.sleep(1)
+        time.sleep(1)
     print("Time is up")
-
-def countdown30(countdownsec):
-
-
-    seconds = 30
-
-    for i in range(seconds):
-        print(str(seconds-i) + " seconds remaining \n")
-
-    time.sleep(1)
-    print("Time is up")
-
-def countdown15(countdownsec):
+def countdown15rest(countdownsec):
 
 
     seconds = 15
+    print("Please allow your wrist to rest for 15 seconds.")
 
     for i in range(seconds):
         print(str(seconds-i) + " seconds remaining \n")
+        time.sleep(1)
+    p.stop()
+    print("The resting period has ended.")
 
-    time.sleep(1)
+def countdown30(countdownsec):
+
+    p.play()
+    seconds = 30
+    
+    
+    for i in range(seconds):
+        print(str(seconds-i) + " seconds remaining \n")
+        time.sleep(1)
+    
     print("Time is up")
 
 
 
-def warm_up():
+def warm_up(countdownsec):
     warmup_repeat = 0
 
     if warmup_repeat <= 4:
         print("Please perform a warm-up before we begin:")
-        im = Image.open("ctwarmup.webp")
-        im.show()
-        countdown15(countdownsec)
+        startfile('WarmUpTrim.mp4')
+        warmupvideotimer(countdownsec)
+        countdown15rest(countdownsec)
     else:
         print("Please schedule a good time for us to begin our stretch session:")
         time_hourmin = input("When would you like to schedule our next session?:")
         time_hourmin = time(time_hourmin)
     
 
-def scheduled_stretch():
+def scheduled_flexor(countdownsec):
     flex_repeat = 0
-    extend_repeat = 0
 
-    if  flex_repeat <= 4:
+
+    if  flex_repeat < 4:
      print("Please complete a Wrist flexor stretch:")
-     im2 = Image.open("ctwristflexorstretch.jpeg")
-     im2.show()
-     countdown60(countdownsec)
-     countdown15(countdownsec)
+     os.system('open WristFlexorStretchVid.mp4')
+     countdown30(countdownsec)
+     countdown15rest(countdownsec)
      flex_repeat += 1
-    if extend_repeat <= 4:
+
+def scheduled_extensor(countdownsec):
+    extend_repeat = 0
+    
+    if extend_repeat < 4:
      print("Please complete a Wrist extensor stretch:")
-     im3 = Image.open("ctwristextensorstretch.jpeg")
-     im3.show()
-     countdown60(countdownsec)
-     countdown15(countdownsec)
+     os.system('open WristExtensorStretchVid.mp4')
+     countdown30(countdownsec)
+     countdown15rest(countdownsec)
      extend_repeat += 4
 
-if current_time == "09:00:00":
-    warm_up()
-    scheduled_stretch()
-elif current_time == "12:00:00":
-    scheduled_stretch()
-elif current_time == "15:00:00":
-    scheduled_stretch()
-elif current_time == "18:00:00":
-    scheduled_stretch()
-elif current_time == "22:00:00":
-    scheduled_stretch()
+rep = 0
+
+
+while rep < 4:
+    rep += 1
+    if time_hourmin == time.strftime("%H:%M"):
+        warm_up(countdownsec)
+        if rep < 4:
+            scheduled_flexor(countdownsec)
+            scheduled_extensor(countdownsec)
+            rep += 1
+        else:
+            print("Your stretching session is now over. Until next time!")
+    else:
+        print("Your stretching session is now over. Until next time!")
 
